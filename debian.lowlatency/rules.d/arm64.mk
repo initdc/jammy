2a25a447a57ba57a506ed3c890c75f190afe9a00
Paolo Pisati <paolo.pisati@canonical.com>
2022-02-07 15:10:22 +0100

    UBUNTU: Start new release
    Ignore: yes
    Signed-off-by: Paolo Pisati <paolo.pisati@canonical.com>

--- debian.lowlatency/rules.d/arm64.mk
+++ debian.lowlatency/rules.d/arm64.mk
@@ -2,7 +2,6 @@ human_arch	= ARMv8
 build_arch	= arm64
 header_arch	= arm64
 defconfig	= defconfig
-flavours	= generic generic-64k lowlatency lowlatency-64k
 build_image	= Image.gz
 kernel_file	= arch/$(build_arch)/boot/Image.gz
 install_file	= vmlinuz
@@ -20,3 +19,4 @@ do_tools_bpftool = true
 
 do_dtbs		= true
 do_zfs		= true
+flavours	= lowlatency lowlatency-64k
